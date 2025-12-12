4a52d0b428b93c5fd79e624d6aa241e4fcd3e86b
spooky-yt56 <spookyyt560@gmail.com>
2025-12-12 14:38:53 +0300

    Implement BotEventHandler for entity damage interactions

--- src/main/java/com/spooky/pvpbot/boteventhandler.java
+++ src/main/java/com/spooky/pvpbot/boteventhandler.java
@@ -1,1 +1,19 @@
+package com.spooky.pvpbot;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+
+public class BotEventHandler implements ServerEntityEvents.EntityDamageCallback {
+    @Override
+    public boolean interact(LivingEntity entity, DamageSource source, float amount) {
+        if (source.getAttacker() instanceof BotEntity bot && entity instanceof LivingEntity target) {
+            if (target.isDead() || amount <= 0) {
+                bot.onMiss();
+            } else {
+                bot.onSuccessfulHit();
+            }
+        }
+        return true;
+    }
+}
